f620bdec38d8e95f8d2a5a6f1e06465e5c9cbe26
Alexander Nefedov <nefedov.alexander91@yandex.ru>
2022-04-30 18:19:44 +0700

    Prevent invalid uses of super

--- jlox/src/main/java/com/nay/lox/Resolver.java
+++ jlox/src/main/java/com/nay/lox/Resolver.java
@@ -26,6 +26,7 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
   private enum ClassType {
     NONE,
     CLASS,
+    SUBCLASS
   }
 
   @Override
@@ -68,6 +69,19 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
   @Override
   public Void visitSuperExpr(Expr.Super expr) {
+    if (currentClass == ClassType.NONE) {
+      ErrorReporter.reportError(
+          expr.keyword,
+          "Can't use 'super' outside of a class."
+      );
+    }
+    else if (currentClass != ClassType.SUBCLASS) {
+      ErrorReporter.reportError(
+          expr.keyword,
+          "Can't use 'super' in a class with no superclass"
+      );
+    }
+
     resolveLocal(expr, expr.keyword);
     return null;
   }
@@ -147,6 +161,7 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     }
 
     if (stmt.superclass != null) {
+      currentClass = ClassType.SUBCLASS;
       resolve(stmt.superclass);
     }
 
