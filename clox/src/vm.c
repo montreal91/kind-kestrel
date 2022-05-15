33ec79da284e5dd124781f054558901a3f6e3df7
Alexander Nefedov <nefedov.alexander91@yandex.ru>
2022-05-15 12:12:48 +0700

    A Value Stack Manipulator

--- clox/src/vm.c
+++ clox/src/vm.c
@@ -6,10 +6,25 @@
 
 VM vm;
 
+static void resetStack() {
+  vm.stackTop = vm.stack;
+}
+
 void initVM() {
+  resetStack();
 }
 
 void freeVM() {
+}
+
+void push(Value value) {
+  *vm.stackTop = value;
+  vm.stackTop++;
+}
+
+Value pop() {
+  vm.stackTop--;
+  return *vm.stackTop;
 }
 
 static InterpretResult run() {
@@ -18,18 +33,26 @@ static InterpretResult run() {
 
   for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
+    printf("          ");
+    for (Value* slot = vm.stack; slot < vm.stackTop; slot++) {
+      printf("[ ");
+      printValue(*slot);
+      printf(" ]");
+    }
+    printf("\n");
     disassembleInstruction(vm.chunk, (int) (vm.ip - vm.chunk->code));
 #endif // DEBUG_TRACE_EXECUTION
 
     uint8_t instruction;
     switch (instruction = READ_BYTE()) {
       case OP_RETURN: {
+        printValue(pop());
+        printf("\n");
         return INTERPRET_OK;
       }
       case OP_CONSTANT: {
         Value constant = READ_CONSTANT();
-        printValue(constant);
-        printf("\n");
+        push(constant);
         break;
       }
     }
