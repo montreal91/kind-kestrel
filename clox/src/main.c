5eb98f9fd1a284448a23f99894a87a9f3ab553ed
Alexander Nefedov <nefedov.alexander91@yandex.ru>
2022-07-29 18:07:41 +0700

    A Lexical Grammar for Lox

--- clox/src/main.c
+++ clox/src/main.c
@@ -59,33 +59,16 @@ static void runfile(const char* path) {
 
 int main(int argc, const char* argv[]) {
   initVM();
-  Chunk chunk;
 
-  initChunk(&chunk);
-
-  int constant = addConstant(&chunk, 1.2);
-  writeChunk(&chunk, OP_CONSTANT, 123);
-  writeChunk(&chunk, constant, 123);
-
-  constant = addConstant(&chunk, 4.4);
-  writeChunk(&chunk, OP_CONSTANT, 123);
-  writeChunk(&chunk, constant, 123);
-
-  writeChunk(&chunk, OP_ADD, 123);
-
-  constant = addConstant(&chunk, 5.6);
-  writeChunk(&chunk, OP_CONSTANT, 123);
-  writeChunk(&chunk, constant, 123);
-
-  writeChunk(&chunk, OP_DIVIDE, 123);
-  writeChunk(&chunk, OP_NEGATE, 123);
-  writeChunk(&chunk, OP_RETURN, 123);
-
-  disassembleChunk(&chunk, "test chunk");
-  printf("\n");
-  interpret(&chunk);
+  if (argc == 1) {
+    repl();
+  } else if (argc == 2) {
+    runfile(argv[1]);
+  } else {
+    fprintf(stderr, "Usage: clox [path]\n");
+    exit(64);
+  }
 
   freeVM();
-  freeChunk(&chunk);
   return 0;
 }
