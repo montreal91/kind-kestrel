75d519b3cc9d8228f8fca1168234458b3e564a2e
Alexander Nefedov <nefedov.alexander91@yandex.ru>
2022-12-02 01:00:43 +0500

    CLox: Emmitting bytes

--- clox/src/compiler.c
+++ clox/src/compiler.c
@@ -16,7 +16,12 @@ typedef struct {
 } Parser;
 
 Parser parser;
+Chunk* compilingChunk;
 
+
+static Chunk* currentChunk() {
+  return compilingChunk;
+}
 
 static void errorAt(Token* token, const char* message) {
   if (parser.panicMode) return;
@@ -65,8 +70,26 @@ static void consume(TokenType type, const char* message) {
   errorAtCurrent(message);
 }
 
+static void emitByte(uint8_t byte) {
+  writeChunk(currentChunk(), byte, parser.previous.line);
+}
+
+static void emitReturn() {
+  emitByte(OP_RETURN);
+}
+
+static void endCompiler() {
+  emitReturn();
+}
+
+static void emitBytes(uint8_t byte1, uint8_t byte2) {
+  emitByte(byte1);
+  emitByte(byte2);
+}
+
 bool compile(const char* source, Chunk* chunk) {
   initScanner(source);
+  compilingChunk = chunk;
 
   parser.hadError = false;
   parser.panicMode = false;
@@ -74,5 +97,6 @@ bool compile(const char* source, Chunk* chunk) {
   advance();
   // expression();
   consume(TOKEN_EOF, "Expect end of expression.");
+  endCompiler();
   return !parser.hadError;
 }
