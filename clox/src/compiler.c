4b84e3f0bedde0807ac76150eee76dcc41d5acce
Alexander Nefedov <nefedov.alexander91@yandex.ru>
2022-12-04 12:59:32 +0500

    CLox: Value type conversions

--- clox/src/compiler.c
+++ clox/src/compiler.c
@@ -6,6 +6,7 @@
 #include "common.h"
 #include "compiler.h"
 #include "scanner.h"
+#include "value.h"
 
 #ifdef DEBUG_PRINT_CODE
 #include "debug.h"
@@ -166,7 +167,7 @@ static void grouping() {
 
 static void number() {
   double value = strtod(parser.previous.start, NULL);
-  emitConstant(value);
+  emitConstant(NUMBER_VAL(value));
 }
 
 static void binary() {
