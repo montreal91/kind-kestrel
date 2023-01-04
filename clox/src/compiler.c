b13b00e5bb45f4669074bf59b11f7061e0757463
Alexander Nefedov <nefedov.alexander91@yandex.ru>
2023-01-04 20:31:05 +0500

    CLox: Strings

--- clox/src/compiler.c
+++ clox/src/compiler.c
@@ -5,6 +5,7 @@
 #include "chunk.h"
 #include "common.h"
 #include "compiler.h"
+#include "object.h"
 #include "scanner.h"
 #include "value.h"
 
@@ -170,6 +171,10 @@ static void number() {
   emitConstant(NUMBER_VAL(value));
 }
 
+static void string() {
+  emitConstant(OBJ_VAL(copyString(parser.previous.start + 1, parser.previous.length - 2)));
+}
+
 static void binary() {
   TokenType operatorType = parser.previous.type;
   ParseRule* rule = getRule(operatorType);
@@ -234,7 +239,7 @@ ParseRule rules[] = {
   [TOKEN_LESS]            = {NULL,      binary,   PREC_COMPARISON},
   [TOKEN_LESS_EQUAL]      = {NULL,      binary,   PREC_COMPARISON},
   [TOKEN_IDENTIFIER]      = {NULL,      NULL,     PREC_NONE},
-  [TOKEN_STRING]          = {NULL,      NULL,     PREC_NONE},
+  [TOKEN_STRING]          = {string,    NULL,     PREC_NONE},
   [TOKEN_NUMBER]          = {number,    NULL,     PREC_NONE},
   [TOKEN_AND]             = {NULL,      NULL,     PREC_NONE},
   [TOKEN_CLASS]           = {NULL,      NULL,     PREC_NONE},
