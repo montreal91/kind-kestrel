c36a87819db17902e523e829acf89d3047c03c27
Alexander Nefedov <nefedov.alexander91@yandex.ru>
2023-01-07 17:49:57 +0500

    CLox: Block Statements

--- clox/src/compiler.c
+++ clox/src/compiler.c
@@ -149,6 +149,14 @@ static void endCompiler() {
 #endif
 }
 
+static void beginScope() {
+  current->scopeDepth++;
+}
+
+static void endScope() {
+  current->scopeDepth--;
+}
+
 static void expression();
 static void statement();
 static void declaration();
@@ -194,6 +202,14 @@ static void defineVariable(uint8_t global) {
 
 static void expression() {
   parsePrecedence(PREC_ASSIGNMENT);
+}
+
+static void block() {
+  while (!check(TOKEN_RIGHT_BRACE) && !check(TOKEN_EOF)) {
+    declaration();
+  }
+
+  consume(TOKEN_RIGHT_BRACE, "Expect '}' after block.");
 }
 
 static void varDeclaration() {
@@ -256,6 +272,10 @@ static void declaration() {
 static void statement() {
   if (match(TOKEN_PRINT)) {
     printStatement();
+  } else if (match(TOKEN_LEFT_BRACE)) {
+    beginScope();
+    block();
+    endScope();
   } else {
     expressionStatement();
   }
