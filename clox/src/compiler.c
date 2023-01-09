2947876fa178c63195f715fd50d9bf8ca0c816e7
Alexander Nefedov <nefedov.alexander91@yandex.ru>
2023-01-09 18:23:44 +0500

    CLox: Compiling to Function Objects

--- clox/src/compiler.c
+++ clox/src/compiler.c
@@ -49,7 +49,15 @@ typedef struct {
   int depth;
 } Local;
 
+typedef enum {
+  TYPE_FUNCTION,
+  TYPE_SCRIPT,
+} FunctionType;
+
 typedef struct {
+  ObjFunction* function;
+  FunctionType type;
+
   Local locals[UINT8_COUNT];
   int localCount;
   int scopeDepth;
@@ -68,7 +76,7 @@ static void emitBytes(uint8_t byte1, uint8_t byte2);
 
 
 static Chunk* currentChunk() {
-  return compilingChunk;
+  return &current->function->chunk;
 }
 
 static void errorAt(Token* token, const char* message) {
@@ -176,14 +184,20 @@ static uint8_t makeConstant(Value value) {
   return (uint8_t)constant;
 }
 
-static void endCompiler() {
+static ObjFunction* endCompiler() {
   emitReturn();
+  ObjFunction* function = current->function;
 
 #ifdef DEBUG_PRINT_CODE
   if (!parser.hadError) {
-    disassembleChunk(currentChunk(), "code");
+    disassembleChunk(
+      currentChunk(),
+      function->name != NULL ? function->name->chars : "<script>"
+    );
   }
 #endif
+
+  return function;
 }
 
 static void beginScope() {
@@ -504,10 +518,19 @@ static void emitConstant(Value value) {
   emitBytes(OP_CONSTANT, makeConstant(value));
 }
 
-static void initCompiler(Compiler* compiler) {
+static void initCompiler(Compiler* compiler, FunctionType type) {
+  compiler->function = NULL;
+  compiler->type = type;
+
   compiler->localCount = 0;
   compiler->scopeDepth = 0;
+  compiler->function = newFunction();
   current = compiler;
+
+  Local* local = &current->locals[current->localCount++];
+  local->depth = 0;
+  local->name.start = "";
+  local->name.length = 0;
 }
 
 static void grouping(bool canAssign) {
@@ -637,11 +660,12 @@ static ParseRule* getRule(TokenType type) {
   return &rules[type];
 }
 
-bool compile(const char* source, Chunk* chunk) {
+ObjFunction* compile(const char* source) {
   initScanner(source);
   Compiler compiler;
-  initCompiler(&compiler);
-  compilingChunk = chunk;
+  // initCompiler(&compiler);
+  // compilingChunk = chunk;
+  initCompiler(&compiler, TYPE_SCRIPT);
 
   parser.hadError = false;
   parser.panicMode = false;
@@ -652,6 +676,6 @@ bool compile(const char* source, Chunk* chunk) {
     declaration();
   }
 
-  endCompiler();
-  return !parser.hadError;
+  ObjFunction* function = endCompiler();
+  return parser.hadError ? NULL : function;
 }
--- clox/src/vm.c
+++ clox/src/vm.c
@@ -231,7 +231,7 @@ InterpretResult interpret(const char* source) {
   Chunk chunk;
   initChunk(&chunk);
 
-  if (!compile(source, &chunk)) {
+  if (!compile(source)) {
     freeChunk(&chunk);
     return INTERPRET_COMPILE_ERROR;
   }
