8780ba63e9594dde45f40c8494396c09246874b1
Alexander Nefedov <nefedov.alexander91@yandex.ru>
2023-01-09 22:15:42 +0500

    CLox: Function Declarations

--- clox/src/compiler.c
+++ clox/src/compiler.c
@@ -55,6 +55,7 @@ typedef enum {
 } FunctionType;
 
 typedef struct {
+  struct Compiler* enclosing;
   ObjFunction* function;
   FunctionType type;
 
@@ -73,6 +74,7 @@ static void declaration();
 static ParseRule* getRule();
 static void emitByte(uint8_t byte);
 static void emitBytes(uint8_t byte1, uint8_t byte2);
+static void initCompiler(Compiler* compiler, FunctionType type);
 
 
 static Chunk* currentChunk() {
@@ -197,6 +199,7 @@ static ObjFunction* endCompiler() {
   }
 #endif
 
+  current = current->enclosing;
   return function;
 }
 
@@ -305,6 +308,7 @@ static uint8_t parseVariable(const char* errorMessage) {
 }
 
 static void markInitialized() {
+  if (current->scopeDepth == 0) return;
   current->locals[current->localCount - 1].depth = current->scopeDepth;
 }
 
@@ -347,6 +351,40 @@ static void block() {
   }
 
   consume(TOKEN_RIGHT_BRACE, "Expect '}' after block.");
+}
+
+static void function(FunctionType type) {
+  Compiler compiler;
+  initCompiler(&compiler, type);
+  beginScope();
+
+  consume(TOKEN_LEFT_PAREN, "Expect '(' after function name.");
+
+  if (!check(TOKEN_RIGHT_PAREN)) {
+    do {
+      current->function->arity++;
+      if (current->function->arity > 255) {
+        errorAtCurrent("Can't have more than 255 parameters.");
+      }
+
+      uint8_t constant = parseVariable("Expect parameter name.");
+      defineVariable(constant);
+    } while (match(TOKEN_COMMA));
+  }
+
+  consume(TOKEN_RIGHT_PAREN, "Expect ')' after parameters.");
+  consume(TOKEN_LEFT_BRACE, "Expect '{' before function body.");
+  block();
+
+  ObjFunction* function = endCompiler();
+  emitBytes(OP_CONSTANT, makeConstant(OBJ_VAL(function)));
+}
+
+static void funDeclaration() {
+  uint8_t global = parseVariable("Expect function name.");
+  markInitialized();
+  function(TYPE_FUNCTION);
+  defineVariable(global);
 }
 
 static void varDeclaration() {
@@ -477,9 +515,13 @@ static void synchronize() {
 }
 
 static void declaration() {
-  if (match(TOKEN_VAR)) {
+  if (match(TOKEN_FUN)) {
+    funDeclaration();
+  }
+  else if (match(TOKEN_VAR)) {
     varDeclaration();
-  } else {
+  }
+  else {
     statement();
   }
 
@@ -519,6 +561,7 @@ static void emitConstant(Value value) {
 }
 
 static void initCompiler(Compiler* compiler, FunctionType type) {
+  compiler->enclosing = current;
   compiler->function = NULL;
   compiler->type = type;
 
@@ -526,6 +569,12 @@ static void initCompiler(Compiler* compiler, FunctionType type) {
   compiler->scopeDepth = 0;
   compiler->function = newFunction();
   current = compiler;
+
+  if (type != TYPE_SCRIPT) {
+    current->function->name = copyString(
+      parser.previous.start, parser.previous.length
+    );
+  }
 
   Local* local = &current->locals[current->localCount++];
   local->depth = 0;
@@ -663,8 +712,6 @@ static ParseRule* getRule(TokenType type) {
 ObjFunction* compile(const char* source) {
   initScanner(source);
   Compiler compiler;
-  // initCompiler(&compiler);
-  // compilingChunk = chunk;
   initCompiler(&compiler, TYPE_SCRIPT);
 
   parser.hadError = false;
