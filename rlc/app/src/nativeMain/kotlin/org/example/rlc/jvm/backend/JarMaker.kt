1129a2897e5cad63a71691b3796dd2895fba4108
Alexander Nefedov <nefedov.alexander91@yandex.ru>
2024-06-10 13:23:04 +0500

    RLC: Compiler to Jar. Part 2.
    Done:
    - Minor fixes
    - Analyzed zip file format
    - Localized what's missing
    
    TODO:
    - Fix zip writing algorithm

--- rlc/app/src/nativeMain/kotlin/org/example/rlc/jvm/backend/JarMaker.kt
+++ rlc/app/src/nativeMain/kotlin/org/example/rlc/jvm/backend/JarMaker.kt
@@ -11,21 +11,19 @@ import platform.posix.fopen
 import platform.posix.fwrite
 
 @OptIn(ExperimentalForeignApi::class)
-private fun writeJarEntry(file: CPointer<FILE>, entryName: String, content: ByteArray) {
+private fun writeJarEntry(file: CPointer<FILE>, entryName: String, content: ByteArray): Int {
   val header = ByteArray(30)
   header[0] = 0x50
   header[1] = 0x4B
   header[2] = 0x03
   header[3] = 0x04
-  header[10] = 0x08 // Compression method (deflate)
+  header[10] = 0x00 // Compression method (none)
   val nameBytes = entryName.encodeToByteArray()
   val nameLength = nameBytes.size.toShort()
   val contentLength = content.size
 
   // Write local file header
-//  header[26] = (nameLength and 0xFF).toByte()
   header[26] = nameLength.toBytes()[0]
-//  header[27] = ((nameLength shr 8) and 0xFF).toByte()
   header[27] = nameLength.toBytes()[1]
   header[18] = (contentLength and 0xFF).toByte()
   header[19] = ((contentLength shr 8) and 0xFF).toByte()
@@ -35,6 +33,8 @@ private fun writeJarEntry(file: CPointer<FILE>, entryName: String, content: Byte
   fwrite(header.refTo(0), 1u, header.size.toULong(), file)
   fwrite(nameBytes.refTo(0), 1u, nameBytes.size.toULong(), file)
   fwrite(content.refTo(0), 1u, content.size.toULong(), file)
+
+  return header.size + nameBytes.size + contentLength
 }
 
 @OptIn(ExperimentalForeignApi::class)
@@ -48,19 +48,22 @@ fun createJarFile(classFiles: List<ClassFile>, outputJar: String, mainClass: Str
     val jarFile = fopen(outputJar, "wb") ?: error("Failed to open file for writing")
     try {
       // Write manifest entry
-//      writeJarEntry(jarFile, "META-INF/MANIFEST.MF", manifestContent.encodeToByteArray())
+      writeJarEntry(jarFile, "META-INF/MANIFEST.MF", manifestContent.encodeToByteArray())
 
       // Write class files
+      var writtenBytes = 0
       classFiles.forEach { classFile ->
         try {
-//          val classFileName = classFilePath.substringAfterLast("/")
           val classFileBytes = ClassCompiler().compileClass(classFile)
-          writeJarEntry(jarFile, classFile.filename, classFileBytes)
+          val bytes = writeJarEntry(jarFile, classFile.filename, classFileBytes)
+          writtenBytes += bytes
         }
         catch (e: Throwable) {
           // do nothing
         }
       }
+      // Here should be a sequence of Central Directory File Headers that describe each file in zip/jar
+      // Here "end of central directory" should be written
     } finally {
       fclose(jarFile)
     }
